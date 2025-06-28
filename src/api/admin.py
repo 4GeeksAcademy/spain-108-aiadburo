ac2b6c6186c39b8038426ca59dc742bee083d16f
aiadburo <aiadburo@hotmail.com>
2025-06-28 10:34:30 +0000

    class Updated

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users
+from .models import db, Users, Posts, Media
 
 
 def setup_admin(app):
@@ -11,5 +11,7 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Users, db.session))
     # You can duplicate that line to add mew models
+    admin.add_view(ModelView(Posts, db.session))
+    admin.add_view(ModelView(Media, db.session))
     # admin.add_view(ModelView(YourModelName, db.session))
     
--- src/api/models.py
+++ src/api/models.py
@@ -1,14 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+    first_name = db.Column(db.String, nullable=True)
+    last_name = db.Column(db.String, nullable=True)
+
+    posts = db.relationship('Posts', backref='author', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,5 +20,39 @@ class Users(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+            # Do not serialize the password, its a security breach
         }
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    body = db.Column(db.String)
+    date = db.Column(db.Date, nullable=False)
+    image_url = db.Column(db.String)
+
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'audio', name='media_type'), nullable=False)
+    url = db.Column(db.String, nullable=False)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class CharacterFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class PlanetFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
